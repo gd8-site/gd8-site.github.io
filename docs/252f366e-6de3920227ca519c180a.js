ca9e6455ed00af1d3cc628f5cb2bab9f4b49f072
gd8-site <gd8.site@gmail.com>
2022-08-15 22:15:31 -0300

    fix: ajustes na pipeline

--- docs/252f366e-6de3920227ca519c180a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgd8_inc_novo=self.webpackChunkgd8_inc_novo||[]).push([[228],{2775:function(t,n,c){c.d(n,{hoL:function(){return h}});var a=c(1046);function h(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z"}},{tag:"path",attr:{d:"M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5 8-5v10zm-8-7L4 6h16l-8 5z"}}]})(t)}}}]);
-//# sourceMappingURL=252f366e-6de3920227ca519c180a.js.map
